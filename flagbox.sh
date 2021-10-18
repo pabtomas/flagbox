12fd792cd84294896f2ffd8a55f359ac40d255c7
P. Tomas <ptomas@astr-ptomas.edcs.fr>
2021-10-18 16:41:45 +0200

    [fixing - .flagbox.sh] navigation done

--- flagbox.sh
+++ flagbox.sh
@@ -9,18 +9,20 @@ function flagbox () {
     return 1
   fi
 
-  local ALIAS=0
+  local ALIAS=false
   local CHAIN=""
   local FILE=""
 
+  declare -r FILE_AUTHORIZED='a-zA-Z0-9/_.~-'
+
   if [ ${#} -eq 1 ] && [ "${1}" == "--alias" ]; then
-    ALIAS=1
+    ALIAS=true
   elif [ ${#} -eq 2 ] && [ "${1}" == "--chain" ] \
     && [ ${#2} -le $(eval "${FLAGBOX_SIZE}") ] \
     && [ "${2}" == "$(echo "${2}" | sed 's/[^01]//')" ]; then
       CHAIN="${2}"
   elif [ ${#} -eq 3 ] && [ "${1}" == "--chain" ] && [ "${2}" == "11" ] \
-    && [ "${3}" == "$(echo "${3}" | sed 's/[^a-zA-Z0-9/_.~-]//')" ]; then
+    && [ "${3}" == "$(echo "${3}" | sed 's/[^'"${FILE_AUTHORIZED}"']//')" ]; then
       CHAIN="11"
       FILE="${3}"
   else
@@ -32,22 +34,23 @@ function flagbox () {
 
   declare -r RED=$(tput setaf 1)
   declare -r GREEN=$(tput setaf 2)
+  declare -r YELLOW=$(tput setaf 3)
   declare -r RESET=$(tput sgr0)
 
-  if [ ${ALIAS} -eq 1 ]; then
+  if ${ALIAS}; then
 # --alias {{{1
 
 #   Default user variables {{{2
 
-    [ -f "${HOME}/.flagbox.conf" ] && source "${HOME}/.flagbox.conf"
-
     declare -r DEFAULT_SIZE=3
     declare -r DEFAULT_FLAG_SYMB=","
     declare -r DEFAULT_ACTION_SYMB="?"
-    declare -r DEFAULT_STACKMODE=0
-    declare -r DEFAULT_AUTOWRITE=0
+    declare -r DEFAULT_GENERATEALIASES=true
+    declare -r DEFAULT_BACKUPCONFIRM=true
+    declare -r DEFAULT_STACKMODE=false
+    declare -r DEFAULT_AUTOWRITE=false
     declare -r DEFAULT_AUTOWRITEFILE="${HOME}/.flagbox_autowrite"
-    declare -r DEFAULT_AUTORESTORE=0
+    declare -r DEFAULT_AUTORESTORE=false
     declare -r DEFAULT_AUTORESTOREFILE="${HOME}/.flagbox_autowrite"
 
     if [ ! -v FLAGBOX_SIZE ] || [ "x${FLAGBOX_SIZE}" == "x" ]; then
@@ -61,6 +64,16 @@ function flagbox () {
     if [ ! -v FLAGBOX_ACTION_SYMB ] \
       || [ "x${FLAGBOX_ACTION_SYMB}" == "x" ]; then
         FLAGBOX_ACTION_SYMB="${DEFAULT_ACTION_SYMB}"
+    fi
+
+    if [ ! -v FLAGBOX_GENERATEALIASES ] \
+      || [ "x${FLAGBOX_GENERATEALIASES}" == "x" ]; then
+        FLAGBOX_GENERATEALIASES=${DEFAULT_GENERATEALIASES}
+    fi
+
+    if [ ! -v FLAGBOX_BACKUPCONFIRM ] \
+      || [ "x${FLAGBOX_BACKUPCONFIRM}" == "x" ]; then
+        FLAGBOX_BACKUPCONFIRM=${DEFAULT_BACKUPCONFIRM}
     fi
 
     if [ ! -v FLAGBOX_STACKMODE ] || [ "x${FLAGBOX_STACKMODE}" == "x" ]; then
@@ -97,7 +110,7 @@ function flagbox () {
     FLAGBOX_SIZE="$(printf '%s\n' "${QUOTED[*]}")"
 
     if [ "$(eval "${FLAGBOX_SIZE}")" != \
-      "$(eval "${FLAGBOX_SIZE}" | sed 's/[^0-9]//g')" ]; then
+      "$(eval "${FLAGBOX_SIZE}" | sed 's/[^0-9]//')" ]; then
         echo "${RED}FLAGBOX_SIZE has to be a positive integer${RESET}" >&2
         return 1
     fi
@@ -112,11 +125,10 @@ function flagbox () {
       return 1
     fi
 
-    declare -r PROHIBITED='.!#/~\$%'
-    if [ "${FLAGBOX_FLAG_SYMB}" != \
-      "$(echo "${FLAGBOX_FLAG_SYMB}" | sed 's/['${PROHIBITED}']//g')" ]; then
-        echo "${RED}FLAGBOX_FLAG_SYMB contains prohibited characters:${RESET} ${PROHIBITED}" >&2
-        return 1
+    declare -r PROHIBITED='.!#%'
+    if [ "${FLAGBOX_FLAG_SYMB}" != "$(echo "${FLAGBOX_FLAG_SYMB}" \
+      | sed 's/['${PROHIBITED}']//')" ]; then
+        echo "${YELLOW}Your are highly discouraged to use one of those characters for FLAGBOX_FLAG_SYMB or FLAGBOX_ACTION_SYMB:${RESET} ${PROHIBITED}"
     fi
 
     if [ ${#FLAGBOX_ACTION_SYMB} -ne 1 ]; then
@@ -124,10 +136,9 @@ function flagbox () {
       return 1
     fi
 
-    if [ "${FLAGBOX_ACTION_SYMB}" != \
-      "$(echo "${FLAGBOX_ACTION_SYMB}" | sed 's/['${PROHIBITED}']//g')" ]; then
-        echo "${RED}FLAGBOX_ACTION_SYMB contains prohibited characters:${RESET} ${PROHIBITED}" >&2
-        return 1
+    if [ "${FLAGBOX_ACTION_SYMB}" != "$(echo "${FLAGBOX_ACTION_SYMB}" \
+      | sed 's/['${PROHIBITED}']//')" ]; then
+        echo "${YELLOW}Your are highly discouraged to use one of those characters for FLAGBOX_FLAG_SYMB or FLAGBOX_ACTION_SYMB:${RESET} ${PROHIBITED}"
     fi
 
     if [ "${FLAGBOX_FLAG_SYMB}" == "${FLAGBOX_ACTION_SYMB}" ]; then
@@ -135,15 +146,58 @@ function flagbox () {
       return 1
     fi
 
-    if [ "${FLAGBOX_STACKMODE}" != "0" ] && [ "${FLAGBOX_STACKMODE}" != "1" ]; then
-      echo "${RED}FLAGBOX_STACKMODE should be ${RESET} 0 ${RED}or${RESET} 1" >&2
-      return 1
+    eval "declare -r -a BIN=( $(printf %$(eval "${FLAGBOX_SIZE}")s \
+      | sed 's/ /{0..1}/g') )"
+
+    local DUP=$(echo -e "$(compgen -c \
+      | sort -u)\n$(echo "0 1 00 01 10 11 ${BIN[@]}" \
+      | sed 's/0/'${FLAGBOX_FLAG_SYMB}'/g' \
+      | sed 's/1/'${FLAGBOX_ACTION_SYMB}'/g' | tr ' ' '\n')" | sort | uniq -d)
+    if [ ${#DUP} -gt 0 ]; then
+      echo -e "${YELLOW}Your are highly discouraged to use those characters for FLAGBOX_FLAG_SYMB and FLAGBOX_ACTION_SYMB. Generating aliases with those characters will hide these commands:${RESET}\n${DUP}"
     fi
 
-    if [ "${FLAGBOX_FLAG_SYMB}" == "?" ] \
-      || [ "${FLAGBOX_ACTION_SYMB}" == "?" ]; then
-        alias ???="\???"
-        alias ????="\????"
+    if [ "${FLAGBOX_STACKMODE}" != "false" ] \
+      && [ "${FLAGBOX_STACKMODE}" != "true" ]; then
+        echo "${RED}FLAGBOX_STACKMODE should be ${RESET} true ${RED}or${RESET} false" >&2
+        return 1
+    fi
+
+    if [ "${FLAGBOX_GENERATEALIASES}" != "false" ] \
+      && [ "${FLAGBOX_GENERATEALIASES}" != "true" ]; then
+        echo "${RED}FLAGBOX_GENERATEALIASES should be ${RESET} true ${RED}or${RESET} false" >&2
+        return 1
+    fi
+
+    if [ "${FLAGBOX_BACKUPCONFIRM}" != "false" ] \
+      && [ "${FLAGBOX_BACKUPCONFIRM}" != "true" ]; then
+        echo "${RED}FLAGBOX_BACKUPCONFIRM should be ${RESET} true ${RED}or${RESET} false" >&2
+        return 1
+    fi
+
+
+    if [ "${FLAGBOX_AUTOWRITE}" != "false" ] \
+      && [ "${FLAGBOX_AUTOWRITE}" != "true" ]; then
+        echo "${RED}FLAGBOX_AUTOWRITE should be ${RESET} true ${RED}or${RESET} false" >&2
+        return 1
+    fi
+
+    if [ "${FLAGBOX_AUTOWRITEFILE}" != "$(echo "${FLAGBOX_AUTOWRITEFILE}" \
+      | sed 's/[^'"${FILE_AUTHORIZED}"']//')" ]; then
+        echo "${RED}FLAGBOX_AUTOWRITEFILE should only contains authorized filename characters:${RESET} ${FILE_AUTHORIZED}" >&2
+        return 1
+    fi
+
+    if [ "${FLAGBOX_AUTORESTORE}" != "false" ] \
+      && [ "${FLAGBOX_AUTORESTORE}" != "true" ]; then
+        echo "${RED}FLAGBOX_AUTORESTORE should be ${RESET} true ${RED}or${RESET} false" >&2
+        return 1
+    fi
+
+    if [ "${FLAGBOX_AUTORESTOREFILE}" != "$(echo "${FLAGBOX_AUTORESTOREFILE}" \
+      | sed 's/[^'"${FILE_AUTHORIZED}"']//')" ]; then
+        echo "${RED}FLAGBOX_AUTORESTOREFILE should only contains authorized filename characters:${RESET} ${FILE_AUTHORIZED}" >&2
+        return 1
     fi
 
 #   }}}
@@ -157,9 +211,6 @@ function flagbox () {
         FLAGBOX[${FLAGBOX[BOX]},${I}]=''
       done
     fi
-
-    eval "declare -r -a BIN=( $(printf %$(eval "${FLAGBOX_SIZE}")s \
-      | sed 's/ /{0..1}/g') )"
 
     for I in $(seq 1 $(eval "${FLAGBOX_SIZE}")); do
       local NAME=$(printf %${I}s | tr ' ' "${FLAGBOX_FLAG_SYMB}")
@@ -204,10 +255,16 @@ function flagbox () {
       flagbox --chain 1
 #   List-flags chain {{{2
     elif [ "${CHAIN}" == "1" ]; then
+      local TEXT=""
       for I in $(seq 1 $(eval "${FLAGBOX_SIZE}")); do
-        echo "$(printf %${I}s \
+        TEXT="${TEXT}$(printf %${I}s \
           | tr ' ' "${FLAGBOX_FLAG_SYMB}") = ${FLAGBOX[${FLAGBOX[BOX]},${I}]}"
-      done;
+        [ ${I} -lt $(eval "${FLAGBOX_SIZE}") ] && TEXT="${TEXT}\n"
+      done
+      local BOXTEXT="[ Box ${FLAGBOX[BOX]}/${FLAGBOX[MAX]} ]"
+      local BAR="$(printf %$(echo -e "0\n$(( ($(echo -e "${TEXT}" | wc -L) \
+        - ${#BOXTEXT} + 1) / 2 ))" | sort -n -r | head -n 1)s | tr ' ' '=')"
+      echo -e "${BAR}${BOXTEXT}${BAR}\n${TEXT}"
 #   }}}
 #   Backup chains {{{2
     elif [ "${CHAIN}" == "11" ]; then
@@ -250,7 +307,11 @@ function flagbox () {
 #     }}}
 #     Save {{{3
       else
-        [ -f ${BACKUP} ] && rm ${BACKUP}
+        if ${FLAGBOX_BACKUPCONFIRM}; then
+          [ -f ${BACKUP} ] && rm -iv ${BACKUP}
+        else
+          [ -f ${BACKUP} ] && rm ${BACKUP}
+        fi
         local TEXT=""
         for I in $(seq 1 $(eval "${FLAGBOX_SIZE}")); do
           TEXT="${TEXT}$(echo "${FLAGBOX[${FLAGBOX[BOX]},${I}]}")\n"
@@ -267,20 +328,22 @@ function flagbox () {
         CONCAT="${CONCAT}x${FLAGBOX[${FLAGBOX[BOX]},${I}]}"
       done
       if [ ${FLAGBOX[BOX]} -lt ${FLAGBOX[MAX]} ]; then
-        (( ${FLAGBOX[BOX]}+=1 ))
+        (( FLAGBOX[BOX]+=1 ))
       elif [ ${#CONCAT} -gt $(eval "${FLAGBOX_SIZE}") ]; then
-        (( ${FLAGBOX[BOX]}+=1 ))
+        (( FLAGBOX[BOX]+=1 ))
         for I in $(seq 1 $(eval "${FLAGBOX_SIZE}")); do
           FLAGBOX[${FLAGBOX[BOX]},${I}]=''
         done
       else
-        ${FLAGBOX[BOX]}=1
+        FLAGBOX[BOX]=1
       fi
+      FLAGBOX[MAX]=$(echo -e "${FLAGBOX[BOX]}\n${FLAGBOX[MAX]}" \
+        | sort -n -r | head -n 1)
     elif [ "${CHAIN}" == "10" ]; then
       if [ ${FLAGBOX[BOX]} -gt 1 ]; then
-        (( ${FLAGBOX[BOX]}-=1 ))
+        (( FLAGBOX[BOX]-=1 ))
       else
-        ${FLAGBOX[BOX]}=${FLAGBOX[MAX]}
+        FLAGBOX[BOX]=${FLAGBOX[MAX]}
       fi
     fi
 
@@ -289,4 +352,9 @@ function flagbox () {
   fi
 }
 
-flagbox --alias
+[ -f "${HOME}/.flagbox.conf" ] && source "${HOME}/.flagbox.conf"
+if [ ! -f "${HOME}/.flagbox.conf" ]; then
+  flagbox --alias
+elif [ -v FLAGBOX_GENERATEALIASES ] && ${FLAGBOX_GENERATEALIASES}; then
+  flagbox --alias
+fi
